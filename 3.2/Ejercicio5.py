cc339f86816f3536f3848e08630ada41024de23c
aeonics <aeonics@live.no>
2025-03-30 22:47:49 -0600

    Ejercicios 5

---
+++ 3.2/Ejercicio5.py
@@ -0,0 +1,17 @@
+#¿Cuál es la salida del siguiente código?
+n = 3
+
+while n > 0:
+    print(n + 1)
+    n -= 1
+else:
+    print(n)
+
+print("\n############")
+#¿Cuál es la salida del siguiente código?
+n = range(4)
+
+for num in n:
+    print(num - 1)
+else:
+    print(num)
