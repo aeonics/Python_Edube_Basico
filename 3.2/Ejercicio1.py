16fd27a83c245229ca6d2a66c3d8434b4c33ea1f
aeonics <aeonics@live.no>
2025-03-31 22:35:47 -0600

    #Crea un bucle for que cuente de 0 a 10, e imprima números impares en la pantalla

--- 3.2/Ejercicio1.py
+++ 3.2/Ejercicio1.py
@@ -1,4 +1,4 @@
 #Crea un bucle for que cuente de 0 a 10, e imprima números impares en la pantalla. Usa el esqueleto de abajo: 
 for i in range(1, 11):
     if i % 2 != 0:##SI el residuo de la division entre 2 es diferente de 0, entonces es impar
-        print(i)
+        print(i)#
