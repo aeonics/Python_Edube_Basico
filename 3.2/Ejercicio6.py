f8f4e224d3a646d707b34fc658d25c691bd2c043
aeonics <aeonics@live.no>
2025-03-31 22:27:28 -0600

    Separe los ejercicios y coloque algunos comentarios

---
+++ 3.2/Ejercicio6.py
@@ -0,0 +1,9 @@
+##Ejercicio 6#¿Cuál es la salida del siguiente código?
+n = range(4) ##0,1,2,3
+
+for num in n:
+    print(num - 1) 
+else:
+    print(num)
+
+#La primer interacion imprime -1, la segunda 0, la tercera 1 y la cuarta 2.
---
+++ 3.2/Ejercicio7.py
@@ -0,0 +1,8 @@
+#Ejercicio 7
+
+
+
+print("\n############")
+#¿Cuál es la salida del siguiente código?
+for i in range(0, 6, 4): ###El primer valor es 0 pero como incrementa en 3 es 0,3 ya que al sumar 3 mas el rango se sale 
+    print(i)
