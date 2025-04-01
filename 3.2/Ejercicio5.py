da7fdd47ba9875333ec09921de438966b538b4af
aeonics <aeonics@live.no>
2025-03-31 22:18:01 -0600

    Se añade un tercer ejemplo

--- 3.2/Ejercicio5.py
+++ 3.2/Ejercicio5.py
@@ -15,3 +15,8 @@ for num in n:
     print(num - 1)
 else:
     print(num)
+
+print("\n############")
+#¿Cuál es la salida del siguiente código?
+for i in range(0, 6, 4): ###El primer valor es 0 pero como incrementa en 3 es 0,3 ya que al sumar 3 mas el rango se sale 
+    print(i)
