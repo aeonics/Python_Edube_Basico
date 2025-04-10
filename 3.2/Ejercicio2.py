7b8ef900ffac57997ee4f8bf1070d1edbbcebd37
aeonics <aeonics@live.no>
2025-04-09 22:38:45 -0600

    Imprime numeros impares usando while

---
+++ 3.2/Ejercicio2.py
@@ -0,0 +1,7 @@
+#Crea un bucle while que cuente de 0 a 10, e imprima números impares en la pantalla. Usa el esqueleto de abajo:
+x = 1
+while x < 11:
+    if x % 2 != 0:##SI el residuo de la division entre 2 es diferente de 0, entonces es impar
+        print(x)## Línea de código.
+    x=x+1  # Línea de código.
+    # Línea de código.
